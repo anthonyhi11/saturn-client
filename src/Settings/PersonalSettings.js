742056b5d60fd551c1eab8b9a284b600296d8e3c
Anthony Hill <antdavhill@gmail.com>
2020-04-17 12:37:56 -0500

    personal settings working

--- src/Settings/PersonalSettings.js
+++ src/Settings/PersonalSettings.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import Header from "../Header/Header";
+import UsersService from "../services/users-service";
+import LoginService from "../services/login-service";
+import TokenService from "../services/token-services";
 
 export default function PersonalSettings() {
+  let [isSuccessful, setIsSuccessful] = useState(false);
+  function handlePersonalChanges(e) {
+    e.preventDefault();
+    console.log("ran");
+    let first_name = e.target.first_name.value;
+    let last_name = e.target.last_name.value;
+    let email = e.target.email.value;
+    let password = e.target.password.value;
+    let password_confirm = e.target.password_confirm.value;
+
+    let personalChanges = {
+      first_name,
+      last_name,
+      email,
+      password,
+      password_confirm,
+    };
+
+    UsersService.updateUser(personalChanges).then((user) => {
+      let credentials = {
+        email,
+        password,
+      };
+      console.log("success");
+      LoginService.loginUser(credentials).then((res) => {
+        TokenService.saveAuthToken(res.authToken);
+        setIsSuccessful(true);
+      });
+    });
+  }
+
   return (
     <>
       <Header />
@@ -32,12 +66,21 @@ export default function PersonalSettings() {
       </nav>
       <section className="settings-changes">
         <h2>Make changes to your personal account</h2>
-        <form className="settings-form">
+        <form
+          className="settings-form"
+          onSubmit={(e) => handlePersonalChanges(e)}
+        >
           <input
             type="text"
-            name="display-name"
-            id="display-name"
-            placeholder="Display Name"
+            name="first_name"
+            id="first_name"
+            placeholder="First Name"
+          />
+          <input
+            type="text"
+            name="last_name"
+            id="last_name"
+            placeholder="Last Name"
           />
           <input type="text" name="email" id="email" placeholder="Email" />
           <input
@@ -48,14 +91,15 @@ export default function PersonalSettings() {
           />
           <input
             type="password"
-            name="password-confirm"
-            id="password-confirm"
+            name="password_confirm"
+            id="password_confirm"
             placeholder="Confirm password"
           />
+          <button className="settings-change-button" type="submit">
+            Make Changes
+          </button>
         </form>
-        <button className="settings-change-button" type="submit">
-          Make Changes
-        </button>
+        {isSuccessful && <p>Success!</p>}
       </section>
     </>
   );
--- src/services/users-service.js
+++ src/services/users-service.js
@@ -23,6 +23,23 @@ const UsersService = {
       !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
     );
   },
+
+  updateUser(update) {
+    return fetch(`${config.API_ENDPOINT}/users/personalsettings`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+        Authorization: `bearer ${TokenService.getAuthToken()}`,
+      },
+      body: JSON.stringify(update),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          return res.json().then((e) => Promise.reject(e));
+        }
+      })
+      .then(TokenService.clearAuthToken());
+  },
 };
 
 export default UsersService;
