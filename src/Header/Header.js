4958abef8a71b8fe473bf767f7fc4b2022b525e0
Anthony Hill <antdavhill@gmail.com>
2020-04-22 16:51:36 -0500

    took off header icon... import being strange

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -16,11 +16,11 @@ export default function Header() {
 
   return (
     <header className="header">
-      <img
-        src="./images/saturn-icon.svg"
+      {/* <img
+        src="../Header/saturn-icon.svg"
         className="saturn-icon"
         alt="saturn-icon"
-      ></img>
+      ></img> */}
       <h1 className="hero-org" onClick={(e) => history.push("/main")}>
         {organizationName}
       </h1>
